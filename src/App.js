1af96220bb7d9d569c5a5992fae9d099cc11ec4b
clesioks <kornalewski@outlook.com>
2023-03-25 09:52:31 -0300

    begin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Cell from "./components/Cell.js";
+import { useState } from "react";
 
-function App() {
+
+const App = () => {
+
+  const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""])
+
+  console.log(cells)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="gameboard">
+        <Cell />
+      </div>
+      <p></p>
     </div>
   );
 }
