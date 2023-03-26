d6428f34d9b8b2855e7e0dcb312c68a7b815506f
clesioks <kornalewski@outlook.com>
2023-03-26 10:37:10 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,19 +1,66 @@
 import Cell from "./components/Cell.js";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 const App = () => {
 
   const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""])
+  const[go, setGo] = useState('cross')
+  const [winningMessage, setWinningMessage] = useState(null)
 
+  
+  const message = "Agora é a vez do " + go
   console.log(cells)
+
+  const checkScore = () => {
+    const winningCombos = [
+      [0,1,2], [3,4,5], [6,7,8],
+      [0,3,6], [1,4,7], [2,5,8],
+      [0,4,8], [2,4,6]
+    ]
+
+    winningCombos.forEach(array => {
+      let circleWins = array.every(cell => cells[cell] === "circle")
+
+      if (circleWins) {
+        setWinningMessage("Circle Wins")
+        return
+      }
+
+    })
+
+    winningCombos.forEach(array => {
+      let crossWins = array.every(cell => cells[cell] === "cross")
+
+      if (crossWins) {
+        setWinningMessage("Cross Wins")
+        return
+      }
+
+    })
+
+  }
+
+  useEffect(() => {
+    checkScore()
+  }, [cells])
 
   return (
     <div className="app">
       <div className="gameboard">
-        <Cell />
+        {cells.map((cell, index) => 
+        <Cell 
+        key={index} 
+        id={index} 
+        cell={cell} 
+        setCells={setCells} 
+        go={go} 
+        setGo={setGo}
+        cells={cells}
+        winningMessage={winningMessage}
+        />)}
       </div>
-      <p></p>
+      <p>{winningMessage || message}</p>
     </div>
   );
 }
